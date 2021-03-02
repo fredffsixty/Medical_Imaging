f3ea2dc69ae55627fd8eef9cd2e35f04cce5169f
fredffsixty <rpirrone@gmail.com>
2021-03-02 17:16:16 +0100

    Programmazione Python

--- Python programming/hello.py
+++ Python programming/hello.py
@@ -22,7 +22,7 @@ import sys
 # Define a main() function that prints a little greeting.
 def main():
   # Get the name from the command line, using 'World' as a fallback.
-  if len(sys.argv) >= 2:
+  if len(sys.agv) >= 2:
     name = sys.argv[1]
   else:
     name = 'World'
