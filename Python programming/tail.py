fc54821b786b878269d3e3d9e62bdb4cece04bf6
fredffsixty <rpirrone@gmail.com>
2021-03-11 11:42:39 +0100

    Programmazione Python

--- Python programming/tail.py
+++ Python programming/tail.py
@@ -1,3 +1,9 @@
+"""
+tail.py stringa [-c] carattere | [-]numero stampa la sottostringa a partire dall'ultima occorrenza
+di carattere, opzionalmente specificato con il parametro '-c', ovvero stampa la sottostringa a partire
+dalla posizione specificata da numero eventualmente anche negativo
+"""
+
 import sys
 
 
@@ -19,25 +25,35 @@ def lastIndexOf(s, c):
 
 def main():
 
-    if len(sys.argv) != 3 or \
+    if (len(sys.argv) < 3 or len(sys.argv) > 4): 
+        print('Argomenti errati\n\nUtilizzo: tail2.py stringa [-c] carattere | [-]numero')
+        sys.exit()
+    elif len(sys.argv) == 3 and \
         not(sys.argv[2].isdigit() or (sys.argv[2][0]=='-' and sys.argv[2][1::].isdigit())) and \
         len(sys.argv[2])!=1:
-        print('Argomenti errati\n\nUtilizzo: tail2.py stringa carattere | [-]numero')
+        print('Argomenti errati\n\nUtilizzo: tail2.py stringa [-c] carattere | [-]numero')
         sys.exit()
-    elif sys.argv[2].isdigit() or (sys.argv[2][0]=='-' and sys.argv[2][1::].isdigit()):
-        index = int(sys.argv[2])
-        if index < -len(sys.argv[1]) or index >= len(sys.argv[1]):
-            print('Indice specificato fuori dal range\n\n')
-            sys.exit()
-        else:
-            print(sys.argv[1][index::])
+    elif len(sys.argv) == 4 and sys.argv[2]!='-c':
+        print('Argomenti errati\n\nUtilizzo: tail2.py stringa [-c] carattere | [-]numero')
+        sys.exit()
     else:
-        position = lastIndexOf(sys.argv[1],sys.argv[2])
-        if position == len(sys.argv[1]):
-            print(f'Il carattere {sys.argv[2]} non è presente nella stringa\n\n')
-            sys.exit()
+        if sys.argv[2].isdigit() or (sys.argv[2][0]=='-' and sys.argv[2][1::].isdigit()):
+            index = int(sys.argv[2])
+            if index < -len(sys.argv[1]) or index >= len(sys.argv[1]):
+                print('Indice specificato fuori dal range\n\n')
+                sys.exit()
+            else:
+                print(sys.argv[1][index::])
         else:
-            print(sys.argv[1][position::])
+            if sys.argv[2] == '-c':
+                position = lastIndexOf(sys.argv[1],sys.argv[3])
+            else:
+                position = lastIndexOf(sys.argv[1],sys.argv[2])
+            if position == len(sys.argv[1]):
+                print('Il carattere non è presente nella stringa\n\n')
+                sys.exit()
+            else:
+                print(sys.argv[1][position::])
 
 
 if __name__ == '__main__':
